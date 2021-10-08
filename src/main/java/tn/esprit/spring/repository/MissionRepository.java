fe67feaf806567e31dffbbc827c2722156c7945f
firasbelhiba <firas.belhiba@esprit.tn>
2021-10-08 22:45:14 +0100

    Adding the MissionService and its Interface

--- src/main/java/tn/esprit/spring/repository/MissionRepository.java
+++ src/main/java/tn/esprit/spring/repository/MissionRepository.java
@@ -1,10 +1,14 @@
 package tn.esprit.spring.repository;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.Query;
+
 
 import tn.esprit.spring.entities.Mission;
 
 
 public interface MissionRepository extends CrudRepository<Mission, Integer> {
+	@Query(value="select count(*) FROM mission",nativeQuery=true)
+	public float numberMissions();	
 
 }
