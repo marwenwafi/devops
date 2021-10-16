cf140f70f8ec6632e39a764a2e983886b9044e3d
marwenwafi <marwenw@360medlink.com>
2021-10-16 08:50:15 +0100

    Adding tests for timesheet

---
+++ src/test/java/tn/esprit/spring/TimeSheetTest.java
@@ -0,0 +1,59 @@
+package tn.esprit.spring;
+
+import java.util.Date;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import tn.esprit.spring.entities.Employe;
+import tn.esprit.spring.entities.Mission;
+import tn.esprit.spring.entities.Role;
+import tn.esprit.spring.services.EmployeServiceImpl;
+import tn.esprit.spring.services.ITimesheetService;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TimeSheetTest {
+	
+	@Autowired
+	ITimesheetService it;
+	@Autowired
+	EmployeServiceImpl iEmployeService;
+	
+	private static final Logger LOGGER = LogManager.getLogger(TimeSheetTest.class);
+	
+	@Test
+	public void testAjouterTimesheet()
+	{
+		int idemp, idmiss;
+		Employe employe = new Employe( "Marwen", "dhawedi", "marwen.dhawedi1@esprit.tn", true, Role.INGENIEUR);
+		try
+		{
+		idemp = iEmployeService.ajouterEmploye(employe);
+		Mission mission = new Mission("Mission1","Lorem Ipsum");
+		idmiss = it.ajouterMission(mission);
+		it.ajouterTimesheet(idmiss, idemp, new Date(),new Date());
+		LOGGER.info("Added successfully with");
+		}
+		catch (Exception e) { LOGGER.error("Problem encountred : " + e); }
+		
+	}
+	
+	@Test
+	public void testValiderTimesheet()
+	{
+		int idemp, idmiss;
+		Employe employe = new Employe( "Chef", "dhawedi12", "marwen.dhawedi1@esprit.tn", true, Role.CHEF_DEPARTEMENT);
+		idemp = iEmployeService.ajouterEmploye(employe);
+		Mission mission = new Mission("Mission111","Lorem Ipsum");
+		idmiss = it.ajouterMission(mission);
+		it.ajouterTimesheet(idmiss, idemp, new Date(),new Date());
+		it.validerTimesheet(idmiss, idemp, new Date(),new Date(), idemp);
+	}
+
+}
