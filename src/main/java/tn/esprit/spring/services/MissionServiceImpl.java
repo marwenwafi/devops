e7ba0682576fcb1379dfaddafa4d0fcee716a7ae
ghadakhedri1 <ghada.khedri1@esprit.tn>
2021-10-18 13:46:08 +0100

    Code Smell's correction

--- src/main/java/tn/esprit/spring/services/MissionServiceImpl.java
+++ src/main/java/tn/esprit/spring/services/MissionServiceImpl.java
@@ -7,8 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Service;
-
-import tn.esprit.spring.EntrepriseServiceImplTest;
 import tn.esprit.spring.entities.Departement;
 import tn.esprit.spring.entities.Mission;
 import tn.esprit.spring.repository.DepartementRepository;
