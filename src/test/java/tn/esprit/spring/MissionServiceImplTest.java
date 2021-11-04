904deaa8db3e0be663f0b2dd6aa6f92135e93a36
hizem_montassar <44597417+HizemMontassar@users.noreply.github.com>
2021-11-04 17:22:37 +0100

    update tests for departement

--- src/test/java/tn/esprit/spring/MissionServiceImplTest.java
+++ src/test/java/tn/esprit/spring/MissionServiceImplTest.java
@@ -1,14 +1,20 @@
-/*package tn.esprit.spring;
+package tn.esprit.spring;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 import java.text.ParseException;
+import java.util.List;
+import java.util.Optional;
 
 import tn.esprit.spring.entities.Mission;
 import tn.esprit.spring.entities.MissionExterne;
+import tn.esprit.spring.repository.MissionRepository;
 import tn.esprit.spring.services.IMissionService;
 
 
@@ -17,32 +23,26 @@ import tn.esprit.spring.services.IMissionService;
 public class MissionServiceImplTest {
 	@Autowired
 	IMissionService iMissionService;
+	@Autowired
+	MissionRepository missionRepository;
 	
-	@Test
 	public void testAddMissionExterne() throws ParseException{
-		MissionExterne mission = new MissionExterne("Mission1","Lorem Ipsum","mission1@test.com",56);
-		iMissionService.addMission(mission);
 	}
 
 	@Test
 	public void testAddMission() throws ParseException{
 		Mission mission = new Mission("Mission2","Lorem Ipsum dolore");
 		iMissionService.addMission(mission);
-<<<<<<< HEAD
 		assertThat(mission.getName()).isEqualTo("Mission2");
-	} 
+	} */
 	
-		@Test
-=======
-	} 
-	
-	@Test
->>>>>>> parent of 9880eb7 (Merge branch 'main' of https://github.com/marwenwafi/devops)
+	/*	@Test
 	public void testDisplayMissions() throws ParseException{
-		iMissionService.getAllMissions();
+		
+		List<Mission> missions =  iMissionService.getAllMissions(); 
+		assertThat(missions.size()).isEqualTo(10);	
 	}
 	
-<<<<<<< HEAD
 	
 	@Test
 	public void testDeleteMission()  throws Exception {
@@ -50,15 +50,15 @@ public class MissionServiceImplTest {
 		Optional<Mission> deletedMission = missionRepository.findById(2);
 		assertThat(deletedMission).isEmpty();
 		}
-	
+	*/
 	
 	@Test
 	public void testUpdateMission() throws ParseException {
 		iMissionService.updateMissionName(3,"MissionUpdated");
 		assertThat(missionRepository.findById(3).get().getName()).isEqualTo("MissionUpdated");
 	}
-=======
->>>>>>> parent of 9880eb7 (Merge branch 'main' of https://github.com/marwenwafi/devops)
 
+	
+	
 
-}*/
+}
